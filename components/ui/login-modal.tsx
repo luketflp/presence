47bc695f425008bd45874254dd83da4ca3036de2
josué <josueematos15@gmail.com>
2025-06-05 16:39:00 -0300

    background do modal de login de acordo com o tema dark/light

--- components/ui/login-modal.tsx
+++ components/ui/login-modal.tsx
@@ -11,23 +11,51 @@ interface LoginModalProps {
 }
 
 export function Login({ open, onOpenChange }: LoginModalProps) {
-  return (
+  return (  
     <Dialog open={open} onOpenChange={onOpenChange}>
-      <DialogContent className="bg-black text-white max-w-[400px] rounded-md p-6">
+      <DialogContent className="bg-white dark:bg-[#111] max-w-[400px] text-black dark:text-white">
         <h2 className="text-xl font-bold mb-4">Login</h2>
         <div className="space-y-4">
           <div>
             <Label htmlFor="email">Email address</Label>
-            <Input id="email" type="email" placeholder="you@example.com" className="text-white focus:ring-gray-500 focus:border-gray-500 focus:outline-none !ring-gray-500 !border-gray-500" />
+            <Input id="email" type="email" placeholder="you@example.com" className="text-black dark:text-white focus:border-gray-500  focus:ring-gray-500" />
           </div>
           <div>
             <Label htmlFor="password">Password</Label>
-            <Input id="password" type="password" placeholder="Password" className="text-white focus:ring-gray-500 focus:border-gray-500 focus:outline-none !ring-gray-500 !border-gray-500" />
+            <Input id="password" type="password" placeholder="Password" className="text-black dark:text-white focus:ring-gray-500  focus:border-gray-500 " />
           </div>
           <Button className="w-full mt-4">Login</Button>
+
+
+
+
+
+
+{/* 
           <p className="text-sm text-center mt-2 text-muted-foreground">
-            Not a member? <a href="#" className="underline">Signup now</a>
+            Not a member?{" "}
+              <a href="#" className="underline  hover:text-primary/80">
+            Signup now
+          </a>
+          </p> */}
+
+          
+          <p className="text-sm text-center mt-2 text-muted-foreground">
+             
+             <Button    
+            variant="link"
+            className="text-sm text-center mt-2 w-full text-muted-foreground"
+            onClick={() => console.log("Signup now")}
+          >
+           not a member? {"Signup now "}
+          </Button>
           </p>
+
+
+
+
+
+{/* Signup now */}
 
           {/* "Forgot your password?" button" */}
           <Button
